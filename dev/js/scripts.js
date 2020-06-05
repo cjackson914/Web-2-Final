7f5d7a732f02809afba1c3edb09d5da315ce15ab
cjackson914 <63318622+cjackson914@users.noreply.github.com>
2020-06-05 11:50:26 -0400

    smooth scroll and hidden nav

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -23,3 +23,63 @@ $burger.on( "click", mouseClick);
 $navA.on( "click", mouseClick);
 
 
+
+//smooth scroll//
+$("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+   
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+
+
+    var lastScrollTop = 0;
+        header = document.getElementById("header");
+    window.addEventListener("scroll", function(){
+        var scrollTop = window.pageYOffset || document
+            .documentElement.scrollTop;
+        if (scrollTop > lastScrollTop){
+            header.style.top="-100px";
+        } else {
+            header.style.top="0";
+        }
+        lastScrollTop = scrollTop;
+    })
+
+
+    // var lastpos = 0;
+
+    // document.addEventListener('scroll',function(){
+            
+    //     let pos = document.documentElement.scrollTop || document.body.scrollTop;
+    //     let windowHeight = document.documentElement.clientHeight || window.innerHeight;
+    //     let sHeight = document.documentElement.scrollHeight;
+    //     let pWidth = ((pos/(sHeight-windowHeight))*100);
+            
+    //     let top = document.getElementById('header');
+    //     let percent = document.getElementById('header');
+            
+    //     if (pos>top.clientHeight && (pos>=sHeight-windowHeight-100 || lastpos<=pos)){
+    //         top.style.top = (-top.clientHeight)+'px';
+    //     }else{
+    //         top.style.top = 0;
+    //     }
+    
+    //     lastpos = pos;
+            
+    // },true);
